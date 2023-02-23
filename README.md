b2421da61045c86d9e91c51447f4dceb4c262402
spaghetti-guy <9104303+spaghetti-guy@users.noreply.github.com>
2023-02-22 22:07:03 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# merge-audio-and-vtt-subs
-Uses ffmpeg to combine audio, vtt subs, and a static image into an mkv container that is playable with standard video players
+# merge-audio-and-subs
+This is a PowerShell script that takes an image file, an audio file, and a subtitle file as inputs and adds them to an mkv video file using FFmpeg. The audio is not resampled during this process. The result is an mkv video with the orignal audio synced with subtitles that can be played with your video player of choice (tested with MPC-HC and mpv). 
+
+The initial goal of this was to combine .vtt subs with .wav audio, but the script should work for any timestamped subtitles and audio formats supported by FFmpeg
+
+You can use any image for the input, though make sure it's a decent size so the subtitles are legible. There's a 1280x720 solid black image in this repo if you don't care about visuals.
+
+<br>
+
+## Requirements
+* You need ffmpeg.exe somewhere accessible. It shouldn't matter which build you use. For reference, I use the codex full release build at: https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z
+* The script assumes your subtitles have the exact same name as your audio. E.g. ```audio1.wav``` and ```audio1.wav.vtt```. Adujust accordingly. 
+
+<br>
+
+## 1-line command for single file conversions. 
+If you don't want to download a script, you can just run this command in CMD or PowerShell. If you change directories (```cd```) to the location of your inputs, you do not need the full path to them (just their name is fine).
+```
+.\PathToffmpeg.exe -loop 1 -framerate 1 -i PathToYourImage.png -i PathToYourAudio.wav -i PathToYourSubs.vtt -shortest -acodec copy -scodec copy -disposition:s:0 default SomeOutputName.mkv
+```
---
+++ merge-audio-and-subs.ps1
@@ -0,0 +1,88 @@
+# Last modified: 02/22/23 02:48PM
+# Source and readme: https://github.com/spaghetti-guy/merge-audio-and-subs
+#
+# You can pass the variables directly to the script or you can run it interactively. 
+# When calling the script in this way, be sure to surround the paths with quotes if there's spaces
+# E.g. .\merge-audio-and-subs.ps1 -ffmpegPath "C:\path\to\ffmpeg.exe" -contentDirectory "C:\path to\your content\" -imagePath "C:\path\to your\image.png"
+# If you put ffmpeg in the same directory as this script, it will assume you want to use that one.
+
+param($ffmpegPath, $contentDirectory, $imagePath)
+
+# Read input if required and try to correct paths
+if($ffmpegPath -eq $null) {
+    if(Test-Path -Path ffmpeg.exe) {
+        $ffmpegPath = Get-ChildItem -Path .\ -Filter "ffmpeg.exe"
+    } else {
+        Write-Host "`nEnter the path to ffmpeg.exe (do not use quotes):"
+        $ffmpegPath = Read-Host
+    }
+}
+try {$ffmpegPath = Get-ChildItem -Path $ffmpegPath -Filter "ffmpeg.exe" -ErrorAction Stop}
+catch {
+    Write-Host "Error: Path to ffmpeg is invalid" -ForegroundColor Red
+    Write-Host $_ -ForegroundColor Red
+    Return
+}
+
+if($contentDirectory -eq $null) { 
+    Write-Host "`nEnter the path to the directory containing your audio files:"
+    $contentDirectory = Read-Host
+}
+try {
+    if(!(Test-Path -Path $contentDirectory -PathType Container)) {
+        throw
+    }
+}
+catch {
+    Write-Host "Error: Path to your content is invalid" -ForegroundColor Red
+    Write-Host $_ -ForegroundColor Red
+    Return
+}
+
+if($imagePath -eq $null) {
+    Write-Host "`nEnter the path to the image you want to use for the video background:"
+    $imagePath = Read-Host
+}
+try {
+    if(!(Test-Path -Path $imagePath -Include "*.jpg","*.jpeg","*.gif","*.png","*.bmp","*.tif","*.tiff")) {
+        throw
+    }
+}
+catch {
+    Write-Host "Error: Path to your image is inavlid" -ForegroundColor Red
+    Write-Host $_ -ForegroundColor Red
+    Return
+}
+
+
+# Determine format of subtitles
+if(Test-Path -Path $contentDirectory\* -Include "*.vtt") {
+    $subFileFormat = ".vtt"
+} elseif(Test-Path -Path $contentDirectory\* -Include "*.srt") {
+    $subFileFormat = ".srt"
+} elseif(Test-Path -Path $contentDirectory\* -Include "*.ass") {
+    $subFileFormat = ".ass"
+} elseif(Test-Path -Path $contentDirectory\* -Include "*.ssa") {
+    $subFileFormat = ".ssa"
+} elseif(Test-Path -Path $contentDirectory\* -Include "*.sbv") {
+    $subFileFormat = ".sbv"
+}
+
+
+# Silently create an output folder for the videos
+[Void](mkdir $contentDirectory\merged)
+
+
+# Start processing each audio file
+Get-ChildItem -Path $contentDirectory\* -Include "*.wav","*.mp3"."*.flac","*.m4a","*.ogg","*.oga","*.wma" | ForEach-Object {
+    # $audiofile = $_.Name
+    # Write-Host $audiofile
+    $outputName = $_.Name + ".mkv"
+    Write-Host "Creating $contentDirectory\merged\$outputName..."
+    # .\$ffmpegPath -loop 1 -framerate 1 -i $imagePath -i $audiofile -i $audiofile.$subFileFormat -shortest -acodec copy -scodec copy -disposition:s:0 default output.mkv
+    & $ffmpegPath -loglevel quiet -loop 1 -framerate 1 -i $imagePath -i $_.FullName -i $_$subFileFormat -shortest -acodec copy -scodec copy -disposition:s:0 default $contentDirectory\merged\$outputName
+    # Write-Host $_$subFileFormat
+}
+
+
+Write-Host "`nDone!`n" -ForegroundColor Green
